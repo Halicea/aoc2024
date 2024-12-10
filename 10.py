40083b68a44a3cf074d917f8d3f3c983031a5358
halicea <costa@codechem.com>
2024-12-11 00:54:09 +0100

    d10

--- 10.py
+++ 10.py
@@ -9,7 +9,7 @@ def is_oob(pos):
     return i < 0 or j < 0 or i >= len(instr) or j >= len(instr[0])
 
 
-def get_ends(trail: list[tuple[int, int]]) -> list[list[tuple[int, int]]]:
+def find_trails(trail: list[tuple[int, int]]) -> list[list[tuple[int, int]]]:
     i, j = trail[-1]
     num = instr[i][j]
     if num == 9:
@@ -18,7 +18,7 @@ def get_ends(trail: list[tuple[int, int]]) -> list[list[tuple[int, int]]]:
     for neighb in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
         di, dj = neighb
         if not is_oob(neighb) and instr[di][dj] == num + 1:
-            res = get_ends(trail + [neighb])
+            res = find_trails(trail + [neighb])
             for r in res:
                 if not r:
                     continue
@@ -38,9 +38,9 @@ p1 = 0
 p2 = 0
 for x in starting:
     endings = {}
-    trails = get_ends([x])
+    trails = find_trails([x])
     p2 += len(trails)
-    for y in get_ends([x]):
+    for y in find_trails([x]):
         if y[-1] not in endings:
             endings[y[-1]] = []
         endings[y[-1]].append(y)
