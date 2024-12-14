d73963efe6078650dcead787f599f563b073c11f
halicea <costa@codechem.com>
2024-12-14 12:41:15 +0100

    d14

--- 14.py
+++ 14.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 
-f = open(__import__("sys").argv[1])
+instr = open(__import__("sys").argv[1]).read().splitlines()
 
 
 @dataclass
@@ -21,39 +21,23 @@ def pmap(tpl):
     v = [int(x) for x in tpl[1].split(",")]
     return Robot(P(*p), P(*v))
 
-space = P(101, 103)
-quads = [
-    [P(-1, -1), P(space.x // 2, space.y // 2)],
-    [P(space.x // 2, -1), P(space.x, space.y // 2)],
-    [P(-1, space.y // 2), P(space.x // 2, space.y)],
-    [P(space.x // 2, space.y // 2), P(space.x, space.y)]]
 
 def in_quad(p, q):
-    return q[0].x < p.x < q[1].x and q[0].y < p.y < q[1].y
+    top_left, bottom_right = q
+    return top_left.x < p.x < bottom_right.x and top_left.y < p.y < bottom_right.y
 
 
-def pp(*points):
-    for i in range(space.y):
-        for j in range(space.x):
-            c = len([x for x in points if x == P(j, i)])
-            if c > 0:
-                print("#", end="")
-            else:
-                print(" ", end="")
-        print()
-    print()
-
-
-robots = [
-    pmap(x.replace("p=", "").replace("v=", "").split(" "))
-    for x in f.read().splitlines()
+robots = [pmap(x.replace("p=", "").replace("v=", "").split(" ")) for x in instr]
+space = P(101, 103)
+quads = [
+    (P(-1, -1), P(space.x // 2, space.y // 2)),
+    (P(space.x // 2, -1), P(space.x, space.y // 2)),
+    (P(-1, space.y // 2), P(space.x // 2, space.y)),
+    (P(space.x // 2, space.y // 2), P(space.x, space.y)),
 ]
-def pt():
-    pp(*[x.p for x in robots])
-
-print("Initial state:")
-seconds = 10000
-pt()
+seconds = 100000
+p1 = 0
+p2 = 0
 for i in range(seconds):
     for r in robots:
         x = r.v.x + r.p.x
@@ -67,24 +51,29 @@ for i in range(seconds):
         elif y >= space.y:
             y = y - space.y
         r.p = P(x, y)
-    
 
     colision = False
     for r in robots:
         if len([x for x in robots if x.p == r.p]) > 1:
             colision = True
             break
-    if not colision:
-        print("After", i + 1, "seconds:")
-        pt()
-qcount = [0, 0, 0, 0]
-for r in robots:
-    for i, q in enumerate(quads):
-        if in_quad(r.p, q):
-            qcount[i] += 1
-print(qcount)
-p1 = 1
-for x in qcount:
-    p1 *= x
-print("p1", p1)
 
+    if not colision:
+        p2 = i + 1
+        if p1:
+            break
+
+    if i == 99:
+        qcount = [0, 0, 0, 0]
+        for r in robots:
+            for i, q in enumerate(quads):
+                if in_quad(r.p, q):
+                    qcount[i] += 1
+        p1 = 1
+        for x in qcount:
+            p1 *= x
+        if p2:
+            break
+
+print("p1", p1)
+print("p2", p2)
